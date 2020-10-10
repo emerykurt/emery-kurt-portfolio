de4b5af02d0944e665d3af33f76bf059e39b45ac
emerykurt <emerylumsden@gmail.com>
2020-10-10 18:05:48 -0400

    cleanned up tech page

--- src/Tech/JavaScriptProject/JavaScriptContainer.js
+++ src/Tech/JavaScriptProject/JavaScriptContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './Card'
+
 export default function JavaScriptContainer() {
     return (
         <div>
--- src/Tech/ReactProject/ReactContainer.js
+++ src/Tech/ReactProject/ReactContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from './Card'
-import Iframe from 'react-iframe'
+// import Iframe from 'react-iframe'
  
 export default function ReactContainer() {
     return (
