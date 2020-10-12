14af842419d3d175f4a3f913810f08478dae09b5
emerykurt <emerylumsden@gmail.com>
2020-10-12 10:19:32 -0400

    updated

--- src/Makeup/Makeup.js
+++ src/Makeup/Makeup.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import ReactPlayer from 'react-player';
+import video from './Wedding/media/styledShoot.mp4'
 
 export default function Makeup() {
     return (
-        <div>
-            <br/><br/><br/><br/><br/>
-            <h1>Coming Soon!</h1>
+        <div style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <br/><br/>
+            <ReactPlayer align= 'center' playing url={video} />
         </div>
     )
 }
+// https://youtu.be/7gimJVxNs1M
